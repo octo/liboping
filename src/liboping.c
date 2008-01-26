6a80978c319f04a73f3aa8303f697705cf753898
Florian Forster <octo@verplant.org>
2008-01-26 14:00:02 +0100

    src/liboping.c: Return the number of bytes actually required to store the username
    Including the terminating NULL-byte.

--- src/liboping.c
+++ src/liboping.c
@@ -1269,7 +1269,7 @@ int ping_iterator_get_info (pingobj_iter_t *iter, int info,
 	{
 		case PING_INFO_USERNAME:
 			ret = ENOMEM;
-			*buffer_len = strlen (iter->username);
+			*buffer_len = strlen (iter->username) + 1;
 			if (orig_buffer_len <= *buffer_len)
 				break;
 			/* Since (orig_buffer_len > *buffer_len) `strncpy'
