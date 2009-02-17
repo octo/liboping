6ae241903b0605677e104e0a57a0c44df24939c7
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-02-17 13:15:39 +0100

    src/liboping.c: Fix a (too conservative) off-by-one error in ping_iterator_get_info()
    Signed-off-by: Florian Forster <octo@leeloo.lan.home.verplant.org>

--- src/liboping.c
+++ src/liboping.c
@@ -1292,7 +1292,7 @@ int ping_iterator_get_info (pingobj_iter_t *iter, int info,
 		case PING_INFO_HOSTNAME:
 			ret = ENOMEM;
 			*buffer_len = strlen (iter->hostname) + 1;
-			if (orig_buffer_len <= *buffer_len)
+			if (orig_buffer_len < *buffer_len)
 				break;
 			/* Since (orig_buffer_len > *buffer_len) `strncpy'
 			 * will copy `*buffer_len' and pad the rest of
