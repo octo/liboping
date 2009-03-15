11970f14b437d4f8f62040f5913ecc4d5f4fbb53
Florian Forster <octo@leeloo.lan.home.verplant.org>
2009-03-15 11:24:20 +0100

    src/liboping.c: Add debugging output to the TTL setting code.

--- src/liboping.c
+++ src/liboping.c
@@ -872,11 +872,15 @@ static int ping_set_ttl (pinghost_t *ph, int ttl)
 
 	if (ph->addrfamily == AF_INET)
 	{
-		ret = setsockopt (ph->fd, IPPROTO_IP, IP_TTL, &ttl, sizeof (ttl));
+		dprintf ("Setting TTLv4 to %i\n", ttl);
+		ret = setsockopt (ph->fd, IPPROTO_IP, IP_TTL,
+				&ttl, sizeof (ttl));
 	}
 	else if (ph->addrfamily == AF_INET6)
 	{
-		ret = setsockopt (ph->fd, IPPROTO_IPV6, IPV6_UNICAST_HOPS, &ttl, sizeof (ttl));
+		dprintf ("Setting TTLv6 to %i\n", ttl);
+		ret = setsockopt (ph->fd, IPPROTO_IPV6, IPV6_UNICAST_HOPS,
+				&ttl, sizeof (ttl));
 	}
 
 	return (ret);
