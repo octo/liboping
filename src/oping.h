4d7e7d999847a3e042ca834d7f3ad7f1112182d5
Florian Forster <octo@verplant.org>
2014-01-16 07:17:46 +0100

    Try to come up with a saner API for liboping.
    The current API has a coupe of shortcomings. They include:
    * The ping_send() call is synchronous.
    * All hosts are always pinged in the same interval.
    * Duplicates cannot be handled.
    * Responses arriving after the timeout cannot be handled.
    
    This tries to come up with a more asynchronous API which addresses these
    problems:
    * The new oping_send() returns immediately after sending the echo request.
    * When an echo response is received, the given callback function is called. The
      call contains per-packet information (latency, ttl, …) and a pointer to the
      oping_t* object. This can be used to query static information (host name, IP
      address, …).
    * Duplicates are simply marked by a boolean in the per-packet information.

--- src/oping.h
+++ src/oping.h
@@ -94,6 +94,80 @@ const char *ping_get_error (pingobj_t *obj);
 void *ping_iterator_get_context (pingobj_iter_t *iter);
 void  ping_iterator_set_context (pingobj_iter_t *iter, void *context);
 
+/*
+ * Asynchronous interface
+ */
+struct oping_s;
+typedef struct oping_s oping_t;
+
+/* Options passed to oping_create() */
+struct oping_options_s
+{
+	/* Number of sequence numbers to keep in memory. This is used to detect
+	 * duplicates and effectively defined the timeout. For example, if you
+	 * set this to 100 and send two echo request per second, the timeout
+	 * effectively becomes 100 * 0.5 = 50 seconds. Values greater than
+	 * 65536 (2^16) don't make sense and are capped. Defaults to 256. */
+	unsigned int backlog;
+};
+typedef struct oping_options_s oping_options_t;
+
+/* Per-packet information. This is passed to the callback function to inform
+ * about a received echo response. */
+struct oping_sample_s
+{
+	oping_t *obj;
+	/* Round-trip time in seconds */
+	double   latency;
+	/* If true, another response for the same sequence number was
+         * previously received. */
+	_Bool    duplicate;
+	/* Sequence number of this response */
+	uint16_t sequence;
+	/* Time-to-live of the received IP packet. */
+	uint8_t  ttl;
+	/* Quality of Service byte of the received IP packet. */
+	uint8_t  qos;
+};
+typedef struct oping_sample_s oping_sample_t;
+
+/* Creates a new oping_t* object. */
+oping_t *oping_create (char const *node, /* node / host to ping. */
+		oping_options_t const *options, /* NULL for default */
+		int (*callback) (oping_sample_t *, void *user_data),
+		void *user_data);
+/* Destroys a oping_t* object. */
+void oping_destroy (oping_t *obj);
+
+/* Increases the sequence number and sends an ICMP echo request to the node. */
+int oping_send (oping_t *obj);
+
+/* Query various static information. oping_get_hostname() and
+ * oping_get_address() return a pointer to memory inside the oping_t* object.
+ * Their reply must not be freed. */
+/* If "resolved" is false, return hostname as provided by the user to
+ * oping_create(). If true, returns the reverse lookup of the IP address if
+ * available and the user provided name if not. */
+char const   *oping_get_hostname      (oping_t *obj, _Bool resolved);
+char const   *oping_get_address       (oping_t *obj);
+int           oping_get_addrfamily    (oping_t *obj);
+uint16_t      oping_get_ident         (oping_t *obj);
+unsigned long oping_get_sent          (oping_t *obj);
+unsigned long oping_get_received      (oping_t *obj);
+unsigned long oping_get_duplicates    (oping_t *obj);
+
+/*
+ * Potential extentions:
+ *
+ * Timer, sends requests periodically and keeps the user from programming their
+ * own event loop.
+ *
+ * oping_timer_create(double interval)
+ * oping_timer_destroy(timer)
+ * oping_timer_add(timer, oping_t*)
+ * oping_timer_remove(timer, char const *hostname);
+ */
+
 #ifdef __cplusplus
 }
 #endif
