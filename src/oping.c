5566e8f16b83699915a3c884a0cbb182082858cc
Florian Forster <ff@octo.it>
2014-09-25 10:39:31 -0700

    Bump version to 1.7.0; Update ChangeLog.

--- src/oping.c
+++ src/oping.c
@@ -1,6 +1,6 @@
 /**
  * Object oriented C module to send ICMP and ICMPv6 `echo's.
- * Copyright (C) 2006-2011  Florian octo Forster <ff at octo.it>
+ * Copyright (C) 2006-2014  Florian octo Forster <ff at octo.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
