564848035ed71ddd9cc54768fa6f6b05b283ee47
Andika Triwidada <andika@gmail.com>
2017-02-06 17:29:14 +0700

    ressurrect less constrained AC_SEARCH_LIB clock_gettime minor rewrite oping.c

--- src/oping.c
+++ src/oping.c
@@ -1628,8 +1628,10 @@ static void update_host_hook (pingobj_iter_t *iter, /* {{{ */
 		host_get_clock_service (mach_host_self (), CALENDAR_CLOCK, &cclock);
 		cret = clock_get_time (cclock, &mts);
 		mach_port_deallocate (mach_task_self (), cclock);
-		ts.tv_sec = mts.tv_sec;
-		ts.tv_nsec = mts.tv_nsec;
+		ts = (struct timespec) {
+			.tv_sec = mts.tv_sec,
+			.tv_nsec = mts.tv_nsec,
+		};
 #else	
 		cret = clock_gettime (CLOCK_REALTIME, &ts);
 #endif
